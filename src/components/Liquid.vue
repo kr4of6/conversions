c88d3ffc43cb91c750cb5e7e45f6b9089c8027ee
Brian Rasmussen <brian.rasmussen55@gmail.com>
2018-02-28 20:47:43 -0700

    missing math

--- src/components/Liquid.vue
+++ src/components/Liquid.vue
@@ -1,18 +1,39 @@
 <template>
     <div class="home">
-	<h1>Kitchen Conversions</h1>
-	<h2>{{msg}}</h2>
+	<!-- <h1>Kitchen Conversions</h1> -->
+<p>Convert </p>
+<input v-model.number="msg" type="number">
+
+        <select v-model="selected">
+            <!-- <option disabled value=""> Please choose measursement Type</option> -->
+            <option>Liter</option>
+            <option>Fluid Ounces</option>
+            <option>Gallon</option>
+            <option>Pint</option>
+            <option>Cup</option>
+            </select>
+<label> to </label>
+            <select v-model="selected2">
+            <!-- <option disabled value="">Choose</option> -->
+            <option>Liter</option>
+            <option>Fluid Ounces</option>
+            <option>Gallon</option>
+            <option>Pint</option>
+            <option>Cup</option>
+            </select>
     </div>
 </template>
 
 <script>
 export default {
   name: "Liquid",
-  data() {
-    return {
-      msg: "lol"
-    };
-  }
+    data() {
+      return {
+        selected:'',
+        selected2:'',
+        msg: ''
+      };
+    }
 };
 </script>
 
