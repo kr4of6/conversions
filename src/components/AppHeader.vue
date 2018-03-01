8b53db2c112a683dae6e760ef48d5c6942b18601
Brian Rasmussen <brian.rasmussen55@gmail.com>
2018-02-28 20:16:02 -0700

    menu

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,77 @@
+<template>
+   <nav>
+    <label for="show-menu" class="show-menu">Show Menu</label>
+    <input type="checkbox" id="show-menu" role="button">
+       <ul id="menu">
+        <li><router-link active-class="active" to="/" exact>Home</router-link></li>
+        <li><router-link active-class="active" to="/Liquid" exact>Liquid</router-link></li>
+        <!-- <li><router-link active-class="active" to="/profile">Profile</router-link></li> -->
+
+    </ul>
+   </nav>
+</template>
+
+<script>
+export default {
+    name: 'AppHeader'
+}
+</script>
+
+<style scoped>
+/*Strip the ul of padding and list styling*/
+.clear {
+    float: clear;
+}
+nav {
+    display: grid;
+}
+ul {
+    list-style-type:none;
+    margin:0;
+    padding:0;
+}
+
+/*Create a horizontal list with spacing*/
+li {
+    display:inline-block;
+    float: left;
+    margin-right: 1px;
+}
+/*Style for menu links*/
+li a {
+    display:block;
+    margin-right: 20px;
+    /* height: 10px; */
+    text-align: center;
+    color: #666;
+    text-decoration-line: none;    
+    padding: 5px;
+}
+/*Active color*/
+li a.active {
+    color: #FF5035;
+}
+/*Hover state for top level links*/
+li:hover a {
+    border-style: solid;
+    border-color: #666;
+    border-width: 2px;
+}
+/*Style 'show menu' label button and hide it by default*/
+.show-menu {
+    text-decoration: none;
+    color: #fff;
+    background: #FF5035;
+    text-align: center;
+    padding: 10px 0;
+    display: none;
+}
+/*Hide checkbox*/
+input[type=checkbox]{
+    display: none;
+}
+/*Show menu when invisible checkbox is checked*/
+input[type=checkbox]:checked ~ #menu{
+    display: block;
+}
+</style>
---
+++ src/components/Liquid.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="home">
+	<h1>Kitchen Conversions</h1>
+	<h2>{{msg}}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "Liquid",
+  data() {
+    return {
+      msg: "lol"
+    };
+  }
+};
+</script>
+
+<style scoped>
+body {
+  padding: 0px;
+  margin: 0px;
+}
+.home {
+  text-align: center;
+}
+h1 {
+  font-size: 100px;
+  letter-spacing: 0.2rem;
+  color: black;
+  margin-bottom: 40px;
+}
+img {
+  width: 50%;
+}
+</style>
