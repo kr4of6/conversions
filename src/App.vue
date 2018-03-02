6f143e404310434886878df365547e67c80d4bb5
Brian Rasmussen <brian.rasmussen55@gmail.com>
2018-03-01 20:14:35 -0700

    i hate this

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,34 @@
 <template>
   <div id="app">
-    <app-header></app-header>
-    <router-view/>
+    <app-header  ></app-header>
+    <router-view ></router-view>
+    <app-footer id="bottom"></app-footer>
   </div>
 </template>
 
 <script>
-import AppHeader from './components/AppHeader.vue'
+import AppHeader from "./components/AppHeader.vue";
+import AppFooter from "./components/AppFooter.vue";
 export default {
-  name: 'App',
-  components: {AppHeader}
-}
+  name: "App",
+  components: { AppHeader, AppFooter }
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 30px;
+}
+
+#bottom {
+  position: absolute;
+  bottom: 0;
+  text-align: center;
+  width: 100%;
 }
 </style>
