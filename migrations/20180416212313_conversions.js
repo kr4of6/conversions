2972ea165147d667cd7ff915e54e8bd61829c74c
Kathryn Rasmussen <kathryn.rasmussen12@gmail.com>
2018-04-16 21:27:44 -0600

    add user and conversion table

---
+++ migrations/20180416212313_conversions.js
@@ -0,0 +1,18 @@
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+  knex.schema.createTable('conversions', function(table) {
+    table.increments('id').primary();
+    table.string('conversion');
+    table.integer('userID').unsigned().notNullable().references('id').inTable('users');
+
+  }),
+]);
+
+};
+
+exports.down = function(knex, Promise) {
+return Promise.all([
+  knex.schema.dropTable('conversions'),
+]);
+};
